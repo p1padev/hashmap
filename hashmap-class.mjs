459dc56db6d224f68310c8733fa0c21992ac1804
João Vítor Ferraz <jvkferraz@gmail.com>
2024-05-02 14:45:18 -0300

    refactoring

--- hashmap-class.mjs
+++ hashmap-class.mjs
@@ -1,16 +1,32 @@
-import Node from "./node-class.mjs";
+import Node from "./node.mjs";
 
-export default class HashMap {
+class Hashmap {
   constructor() {
     this.buckets = Array.from(new Array(16));
-    this.capacity = 0;
-    this.loadFactor = 0.75;
+    this.numberOfKeysStored = 0;
+    this.loadFactorLimit = 0.75;
+  }
+
+  getCapacity() {
+    return this.buckets.length;
+  }
+
+  getNumberOfKeys() {
+    return this.numberOfKeysStored;
+  }
+
+  getBucket(hash) {
+    return this.buckets[hash % this.buckets.length];
+  }
+
+  setBucket(hash, value) {
+    this.buckets[hash % this.buckets.length] = value;
   }
 
   createHash(key) {
     let hashCode = 0;
-    const primeNumber = 31;
 
+    const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
       hashCode =
         (primeNumber * hashCode + key.charCodeAt(i)) % this.buckets.length;
@@ -19,60 +35,52 @@ export default class HashMap {
     return hashCode;
   }
 
-  set(key, value, isRehashing = false) {
-    const indexOfBucket = this.createHash(key) % this.buckets.length;
-    const node = new Node(key, value);
+  setNode(key, value, isRehashing = false) {
+    const hash = this.createHash(key);
+    const node = new Node();
+    node.key = key;
+    node.value = value;
+    let bucket = this.getBucket(hash);
 
-    if ((this.capacity >= this.loadFactor) & !isRehashing) {
-      this.growMap();
-    }
-
-    if (this.buckets[indexOfBucket] === undefined) {
-      this.buckets[indexOfBucket] = node;
-    } else if (this.buckets[indexOfBucket].key === key) {
-      this.buckets[indexOfBucket].value = value;
-    } else {
-      let currentBucket = this.buckets[indexOfBucket];
-      while (currentBucket != null) {
-        if (currentBucket.next == null) {
-          currentBucket.next = node;
-          break;
-        }
-        if (currentBucket.key === key) {
-          currentBucket.value = value;
-          break;
-        }
-        currentBucket = currentBucket.next;
+    if (!isRehashing) {
+      if (
+        this.numberOfKeysStored >=
+        Math.floor(this.getCapacity() * this.loadFactorLimit)
+      ) {
+        this.growMap();
       }
     }
-
-    this.updateCapacity();
-  }
-
-  get(key) {
-    const indexOfBucket = this.createHash(key) % this.buckets.length;
-    let bucket = this.buckets[indexOfBucket];
 
     while (bucket != null) {
       if (bucket.key === key) {
-        return bucket.value;
+        bucket.value = value;
+        return this;
       }
+      if (bucket.next == null) {
+        bucket.next = node;
+        if (!isRehashing) {
+          this.numberOfKeysStored += 1;
+        }
+        return this;
+      }
+
       bucket = bucket.next;
     }
 
-    return null;
+    this.setBucket(hash, node);
+    if (!isRehashing) {
+      this.numberOfKeysStored += 1;
+    }
+    return this;
   }
 
   has(key) {
-    const newHash = this.createHash(key);
-    const indexOfBucket = newHash % this.buckets.length;
-    let bucket = this.buckets[indexOfBucket];
-
+    const hash = this.createHash(key);
+    let bucket = this.getBucket(hash);
     while (bucket != null) {
       if (bucket.key === key) {
         return true;
       }
-
       bucket = bucket.next;
     }
 
@@ -80,100 +88,79 @@ export default class HashMap {
   }
 
   remove(key) {
-    const index = this.createHash(key) % this.buckets.length;
-    let current = this.buckets[index];
+    const hash = this.createHash(key);
+    let bucket = this.getBucket(hash);
     let prev = null;
 
-    while (current != null) {
-      if (current.key === key) {
+    while (bucket != null) {
+      if (bucket.key === key) {
         if (prev == null) {
-          this.buckets[index] = current.next;
+          this.setBucket(hash, bucket.next);
         } else {
-          prev.next = current.next;
+          prev.next = bucket.next;
         }
-        this.updateCapacity();
+        this.numberOfKeysStored -= 1;
         return true;
       }
-      prev = current;
-      current = current.next;
+
+      prev = bucket;
+      bucket = bucket.next;
     }
+
     return false;
   }
 
-  length() {
-    return this.buckets.reduce((acc, bucket) => {
-      if (bucket != undefined) {
-        while (bucket !== null) {
-          acc += 1;
-          bucket = bucket.next;
-        }
-      }
-      return acc;
-    }, 0);
-  }
-
-  clear() {
-    this.buckets = Array.from(new Array(16));
-    this.updateCapacity();
-  }
-
-  keys() {
-    return this.buckets.reduce((acc, bucket) => {
-      if (bucket != undefined) {
-        while (bucket != null) {
-          acc.push(bucket.key);
-          bucket = bucket.next;
-        }
-      }
-      return acc;
-    }, []);
-  }
-  values() {
-    return this.buckets.reduce((acc, bucket) => {
-      if (bucket != undefined) {
-        while (bucket != null) {
-          acc.push(bucket.value);
-          bucket = bucket.next;
-        }
-      }
-      return acc;
-    }, []);
-  }
-  entries() {
-    return this.buckets.reduce((acc, bucket) => {
-      if (bucket != undefined) {
-        while (bucket != null) {
-          acc.push([bucket.key, bucket.value]);
-          bucket = bucket.next;
-        }
-      }
-      return acc;
-    }, []);
-  }
-
-  updateCapacity() {
-    let totalCount = this.buckets.reduce((acc, bucket) => {
-      let currentBucket = bucket;
-      while (currentBucket != null) {
-        acc += 1;
-        currentBucket = currentBucket.next;
-      }
-      return acc;
-    }, 0);
-
-    this.capacity = totalCount / this.buckets.length;
-    return this;
-  }
-
   growMap() {
-    let oldLength = this.buckets.length;
-    let entries = this.entries();
-    this.buckets = Array.from(new Array(oldLength * 2));
+    const oldEntries = [...this.buckets];
+    const newCapacity = this.getCapacity() * 2;
 
-    entries.forEach(([key, value]) => {
-      this.set(key, value, true);
+    this.buckets = Array.from(new Array(newCapacity));
+
+    oldEntries.forEach((bucket) => {
+      while (bucket != null) {
+        this.setNode(bucket.key, bucket.value, true);
+
+        bucket = bucket.next;
+      }
     });
+  }
 
-    this.updateCapacity();
+  clearMap() {
+    this.buckets = Array.from(new Array(16).fill(null));
+    this.numberOfKeysStored = 0;
+  }
+
+  getKeys() {
+    return this.buckets.reduce((acc, bucket) => {
+      let current = bucket;
+      while (current != null) {
+        acc.push(current.key);
+        current = current.next;
+      }
+      return acc;
+    }, []);
+  }
+
+  getValues() {
+    return this.buckets.reduce((acc, bucket) => {
+      let current = bucket;
+      while (current != null) {
+        acc.push(current.value);
+        current = current.next;
+      }
+      return acc;
+    }, []);
+  }
+
+  getEntries() {
+    return this.buckets.reduce((acc, bucket) => {
+      let current = bucket;
+      while (current != null) {
+        acc.push([current.key, current.value]);
+        current = current.next;
+      }
+
+      return acc;
+    }, []);
   }
 }
